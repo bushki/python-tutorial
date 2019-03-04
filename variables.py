be2c0407c1e8cea02b48d324cf25a17ce099bcf8
Luis M <melgar.luis@gmail.com>
2019-03-03 21:18:33 -0800

    variables

--- variables.py
+++ variables.py
@@ -1,3 +1,32 @@
 '''
 Multi line comment
 '''
+x= 1 # cast as int by default
+y = 2.5 # floating point
+name = 'john' #str 
+is_programmer = True # bool
+
+# multiple assignment
+x, y, name, is_programmer = (10,98.6, 'smith', False)
+
+# output 
+print ('hello python')
+print (name)
+print (x,y,is_programmer)
+
+# math 
+a = x+y
+print (a)
+
+# check type
+print(type(a))
+
+# casting / conversion 
+print('casting')
+y = int(y)
+z = float(y)
+print('type and value', type(z), z)
+
+print(type(x))
+x = str(x)
+print(type(x))
